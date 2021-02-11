3d510d675c9fb2b266599f473863443043e6789e
hideraldobarbosa <hideraldo.barbosa@gmail.com>
2021-02-11 10:10:33 -0300

    primeira intervenção no projeto SallesWebMvc

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web Mvc App from C# Course.";
+            ViewData["Aluno"] = "Hilderaldo Barbosa";
             return View();
         }
 
